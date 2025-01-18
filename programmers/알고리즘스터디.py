f88ce14e8add9130b97437c3d1d7f0c9a79c2b6d
goldangeee <goldangeee@proton.me>
2025-01-18 20:58:11 +0900

    DFS

--- programmers/알고리즘스터디.py
+++ programmers/알고리즘스터디.py
@@ -7,10 +7,28 @@ graph = {
 
 def sol(graph):
     print(1)
-    for x in graph[1]:
-        sol()
-            
+
+    def sol2(graph,x):
+        for a in graph[x]:
+            print(a)
+            if graph[a]:
+                sol2(graph,a)
+    
+    sol2(graph,1)
+        
 sol(graph)
+'''
+def dfs(graph, node):
+    print(node)
+    
+    for nei_node in graph[node]:
+        dfs(graph, nei_node)
+
+def sol(graph):
+    dfs(graph, 1)
+
+sol(graph)
+'''
 # 출력
 # 1
 # 2
