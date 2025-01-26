9b8a05564ba7a6d6c57269c7fe1c2b33485da5cd
goldangeee <goldangeee@proton.me>
2025-01-26 18:29:50 +0900

    250126mysql

---
+++ programmers/알고리즘/32강/문제3.py
@@ -0,0 +1,10 @@
+# 다음과 같은 문자열 리스트를 활용하여 아래의 작업을 수행해주세요.
+
+str_list = ['Python', 'Java', 'C', 'JavaScript', 'Ruby', 'Go']
+# 작업
+# 문자열의 길이를 기준으로 오름차순으로 정렬하고 결과를 출력하세요.
+# str_list.sort(key=len)
+# print(str_list)
+# 문자열을 알파벳 역순으로 정렬하고 결과를 출력하세요.
+str_list.sort(reverse=True)
+print(str_list)
